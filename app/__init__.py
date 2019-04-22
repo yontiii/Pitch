2bb5092cb6085e8ff415cc041b04e6514b79e989
yontey <muasajohn01@gmail.com>
2019-04-22 22:46:27 +0300

    Created flask-script shell

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
     
+    # initializing flask extensions
+    db.init_app(app)
     
     # Creating app configurations
     app.config.from_object(config_options[config_name])
