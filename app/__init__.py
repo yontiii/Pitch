9348c6eaa37de0d3d3da41a10d87ae31565fde65
yontey <muasajohn01@gmail.com>
2019-05-01 18:32:55 +0300

    Added simplemde editor

--- app/__init__.py
+++ app/__init__.py
@@ -5,11 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_simplemde import SimpleMDE
+
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
+simple = SimpleMDE()
 # mail = Mail()
 
 db = SQLAlchemy()
@@ -22,6 +25,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     bootstrap.init_app(app)
+    simple.init_app(app)
     # mail.init_app(app)
     
     # Creating app configurations
--- start.sh
+++ start.sh
@@ -1,7 +1,3 @@
-# export MAIL_USERNAME=muasajohn01@gmail.com
-
-# export MAIL_PASSWORD=john2001
-
-export SECRET_KEY=john123
+export SECRET_KEY=john12345
 
 python3.6 manage.py server
