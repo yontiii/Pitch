b545fda426c108a2dbfafb81c6260e4108d9bef0
yontey <muasajohn01@gmail.com>
2019-05-01 19:04:37 +0300

    defined classes

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
-simple = SimpleMDE()
+# simple = SimpleMDE()
 # mail = Mail()
 
 db = SQLAlchemy()
